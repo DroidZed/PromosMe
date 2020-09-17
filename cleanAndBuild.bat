d6266e2c0313f4491ce301f9ef85d4eb91d86c14
DroidZed <aymen.dragon5@gmail.com>
2020-09-17 20:59:54 +0100

    safety first ladies and gentlemen

--- cleanAndBuild.bat
+++ cleanAndBuild.bat
@@ -7,22 +7,22 @@ cls
 @rem (aka the folder of your app aka the container of index.js and App.js)
 @rem This line is for debug ONLY...not suitable for production:
 
-@rem cd android && .\gradlew clean && cd .. && react-native run-android
+cd android && .\gradlew clean && cd .. && react-native run-android
 
 @rem Another way, is using the custom script I wrote here:
 @rem REMOVE THE @REM clauses and all of the BELOW and comment out
 @rem line 10 by adding @rem in front of it and escape with a space
 
-echo Choose what to do (Clean: C / Release: R / Debug: D)
-set/p "res=>"
-if %res%==R goto RELEASE
-if %res%==D goto DEBUG
-if %res%==C goto CLEAN
-echo Invalid choice.
-:CLEAN
-cd android && .\gradlew clean && cd .. goto END
-:DEBUG
-react-native run-android goto END
-:RELEASE
-cd android && .\gradlew assembleRelease && cd .. goto END
-:END
+@rem echo Choose what to do (Clean: C / Release: R / Debug: D)
+@rem set/p "res=>"
+@rem if %res%==R goto RELEASE
+@rem if %res%==D goto DEBUG
+@rem if %res%==C goto CLEAN
+@rem echo Invalid choice.
+@rem :CLEAN
+@rem cd android && .\gradlew clean && cd .. goto END
+@rem :DEBUG
+@rem react-native run-android goto END
+@rem :RELEASE
+@rem cd android && .\gradlew assembleRelease && cd .. goto END
+@rem :END
